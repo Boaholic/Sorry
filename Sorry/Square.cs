de8e4406f7f41469da16b6f613f91a0a509d7011
jsAcrode <j.s.acrode@gmail.com>
2015-11-15 20:02:08 -0700

    Added Square Basic(Testing Commits)

---
+++ Sorry/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sorry
+{
+    public class Square
+    {
+        private bool occupied;
+        private Pawn occupiedBy;
+        private int ownerPlayer;
+        private color highlighted;
+        public Square()
+        {
+
+        }
+        public Pawn IsOccupied()
+        { 
+            return occupiedBy; 
+        }
+        public Pawn SetOccupied(Pawn newPawn)
+        {
+            return occupiedBy; 
+        }
+    }
+}
