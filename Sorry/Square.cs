81aadd02d83c90c820f6219af22c0a2facd3a412
jsAcrode <j.s.acrode@gmail.com>
2015-11-15 21:10:30 -0700

    IsOccupied needs to be looked at

--- Sorry/Square.cs
+++ Sorry/Square.cs
@@ -12,6 +12,10 @@ namespace Sorry
         {
 
         }
+        //This may need to be changed. If we want it to return a bool 
+        //of occupied instead of returning the pawn it is occupied by
+        //There may need to be a way to say it is not occupied in the
+        //Pawn class
         public Pawn IsOccupied()
         { 
             return occupiedBy; 
