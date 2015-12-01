4102cb52fd702082f5507af8b5e4c3e31d8866dc
Michael <xenophon.of.athens@gmail.com>
2015-12-01 12:16:11 -0700

    adding designer file

---
+++ Sorry/PauseMenu.Designer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorry
+{
+    partial class PauseMenu
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.CancelButton = new System.Windows.Forms.Button();
+
+            //
+            // Cancel Button
+            //
+            this.CancelButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CancelButton.BackColor = System.Drawing.Color.Transparent;
+            this.CancelButton.Font = new System.Drawing.Font("Lucida Calligraphy", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelButton.ForeColor = System.Drawing.Color.Black;
+            this.CancelButton.Location = new System.Drawing.Point(200, 230);
+            this.CancelButton.Name = "cancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(100, 50);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+
+            //
+            // Pause Menu
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(CancelButton);
+        }
+
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
